8c225f16e50fd2975459a5b25ce91e4f818bacce
Carlos Eduardo <carlosestanislaulima@gmail.com>
2022-08-03 21:22:21 -0300

    Começando o desenvolvimento de um todo list

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'Pages/todo_list_page.dart';
 
+void main(app) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TodoListPage(),
+    );
+  }
+}
