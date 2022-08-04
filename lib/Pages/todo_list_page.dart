cd4956f64f884f8564426a8010827a004ff8b766
Carlos Eduardo <carlosestanislaulima@gmail.com>
2022-08-04 12:20:06 -0300

    campo de texto

--- lib/Pages/todo_list_page.dart
+++ lib/Pages/todo_list_page.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key);
+  TodoListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(32),
-          child: TextField(),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicione uma tarefa',
+                    hintText: 'Ex. Estudar',
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff58d385),
+                    padding: EdgeInsets.all(14),
+                  ),
+                  child: Icon(
+                    Icons.add,
+                    size: 30,
+                  ))
+            ],
+          ),
         ),
       ),
     );
